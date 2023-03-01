c5a24f8ba92d4c2f50b33cd4b36d706468e8b697
VictorySamoletova <victoryer@mail.ru>
2023-03-01 14:29:53 +0300

    Finished with constructor explonantion and extands

--- src/constructorTest/Main.java
+++ src/constructorTest/Main.java
@@ -1,22 +1,38 @@
 package constructorTest;
 
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-       Person person = new Person("Sergei","28",'Proletarskaya10',"8800353535")
-Fraction fraction = new Fraction(1,10);
+    public static void main(String[] args) {
+
+
+        Fraction fraction = new Fraction(1, 10);
+
+        Person Oleg = new Person("Konin","Oleg",'m');
+        System.out.println(Oleg);
+    Employee olga = new Employee("Svetova","Olga",'f',"buhgalter");
+    /*olga.setFirstName("Olga");
+    olga.setLastName("Svetova");
+    olga.setGender('f');*/
+
+
+        System.out.println(olga);
+
+        Person olga2 = olga;
+        System.out.println(olga2);
     }
 }
-class Person{
-   private String name;
+/*
+
+class Person {
+    private String name;
     private int age;
 
 
     public Person(String name, int age, String address, String phone) {
         this.name = name;
         this.age = age;
-        
+
 
     }
 
@@ -33,21 +49,103 @@ class Person{
     }
 
     public void setAge(int age) {
-        if(age>0 && age<110)
-        this.age = age;
+        if (age > 0 && age < 110)
+            this.age = age;
     }
 }
+*/
 
- class Fraction{
+class Fraction {
     int numerator;
-    int denominator=1;
+    int denominator = 1;
 
-    public Fraction(int numerator, int denominator){ //это конструктор
-        if(denominator==0){
+    public Fraction(int numerator, int denominator) { //это конструктор
+        if (denominator == 0) {
             System.out.println("can't be zero");
             return;
         }
         this.numerator = numerator;
         this.denominator = denominator;
     }
- }
+}
+
+class Person {
+    protected String firstName;
+    protected String lastName;
+    protected char gender;//m-male; f - female
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+
+    public Person(){
+        this("", "", '-');
+    }
+    public Person(String lastName){
+        this(lastName,"",'-');
+    }
+
+
+
+    public Person(String lastName, String firstName, char gender){
+        this(lastName, firstName);
+        this.gender = gender;
+    }
+public Person(String lastName, String firstName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+}
+
+}
+class Employee extends Person{
+    protected String dolznost;
+
+    public Employee(String dolznost) {
+
+        this.dolznost = dolznost;
+    }
+
+    public Employee(String lastName, String firstName, char gender, String dolznost) {
+        super(lastName, firstName, gender);
+        this.dolznost = dolznost;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "dolznost='" + dolznost + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
